d57dc1cf449c1c2f17d3b6514d63d712980634b0
Yembuluru Rajaneesh <101640141+Rajaneesh2307@users.noreply.github.com>
2025-06-01 16:37:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       message:
-        "Hello from Lambda! I am attaching API to access and automate the process.",
+        "Hello from Lambda! I am attaching API to access and automate the process using Pipeline.",
     }),
   };
 };
